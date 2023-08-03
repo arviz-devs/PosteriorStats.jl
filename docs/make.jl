5d735d4f498d21c00787e6c17aae3e967c94cce9
Seth Axen <seth@sethaxen.com>
2023-08-03 14:14:52 +0200

    Add API page

--- docs/make.jl
+++ docs/make.jl
@@ -10,5 +10,6 @@ makedocs(;
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true", edit_link="main", assets=String[]
     ),
-    pages=["Home" => "index.md"],
+    pages=["Home" => "index.md", "API" => "api.md"],
+    strict=Documenter.except(:footnote, :missing_docs),
 )
