52fab4c174a46543cac01052e84a8feb524bcbf4
Seth Axen <seth@sethaxen.com>
2023-08-04 11:37:40 +0200

    Remove r2_score for InferenceData

--- test/r2_score.jl
+++ test/r2_score.jl
@@ -1,4 +1,3 @@
-using ArviZExampleData
 using GLM
 using PosteriorStats
 using Statistics
@@ -27,23 +26,6 @@ using Test
             # check rough consistency with GLM
             res = lm(@formula(y ~ 1 + x), (; x=Float64.(x), y=Float64.(y)))
             @test r2_val.r2 ≈ r2(res) rtol = 1
-        end
-    end
-
-    @testset "InferenceData inputs" begin
-        @testset for name in ("regression1d", "regression10d")
-            idata = load_example_data(name)
-            VERSION ≥ v"1.9" && @inferred r2_score(idata)
-            r2_val = r2_score(idata)
-            @test r2_val == r2_score(
-                idata.observed_data.y,
-                PermutedDimsArray(idata.posterior_predictive.y, (:draw, :chain, :y_dim_0)),
-            )
-            @test r2_val == r2_score(idata; y_name=:y)
-            @test r2_val == r2_score(idata; y_pred_name=:y)
-            @test r2_val == r2_score(idata; y_name=:y, y_pred_name=:y)
-            @test_throws Exception r2_score(idata; y_name=:z)
-            @test_throws Exception r2_score(idata; y_pred_name=:z)
         end
     end
 end
