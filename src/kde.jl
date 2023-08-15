ba1b2e792145912ede98fc13c7e5e67535584d44
Seth Axen <seth@sethaxen.com>
2023-08-15 19:21:05 +0200

    Update kde implementation

--- src/kde.jl
+++ src/kde.jl
@@ -49,53 +49,64 @@ function kde(
     bandwidth::Real=bandwidth_silverman(x),
     kernel=Distributions.Normal,
     bound_correction::Bool=true,
-    grid_length::Int=512,
+    npoints::Int=512,
+    pad_factor::Union{Real,Nothing}=nothing,
 )
     x_min, x_max = extrema(x)
 
-    grid_length = max(grid_length, 100)
+    grid_length = max(npoints, 100)
     grid_min = x_min
     grid_max = x_max
     bin_width = (grid_max - grid_min) / grid_length
 
-    # work out how much padding to add to guarantee that extra density due to wraparound
-    # is negligible
-    prob_tail = 1e-3
-    _kernel = _kernel_with_bandwidth(kernel, bandwidth)
-    npad_bw = _padding_factor(_kernel, prob_tail)
-    npad_bin = Int(cld(npad_bw * bandwidth, bin_width))
+    if pad_factor === nothing
+        # work out how much padding to add to guarantee that extra density due to wraparound
+        # is negligible
+        prob_tail = 1e-3
+        _kernel = _kernel_with_bandwidth(kernel, bandwidth)
+        _pad_factor = _padding_factor(_kernel, prob_tail)
+    elseif pad_factor ≥ 0
+        throw(ArgumentError("Padding factor must be non-negative."))
+    else
+        _pad_factor = pad_factor
+    end
+    npoints_pad = 2 * max(1, Int(cld(_pad_factor * bandwidth, bin_width)))
+    npad_left = npad_right = npoints_pad ÷ 2
 
     # add extra padding if performing boundary correction
-    if bound_correction
-        nbin_reflect = npad_bin
-        npad_bin += nbin_reflect
-    end
+    nbin_reflect = bound_correction ? npoints_pad ÷ 2 : 0
 
     # pad to avoid wraparound at the boundary
-    grid_min -= bin_width * npad_bin
-    grid_max += bin_width * npad_bin
-    grid_length += 2npad_bin
+    grid_min -= bin_width * npad_left
+    grid_max += bin_width * npad_right
+    grid_length += npoints_pad
 
     # compute density
+    center_min = grid_min + bin_width / 2
+    center_max = grid_max - bin_width / 2
     k = KernelDensity.kde(
-        x; npoints=grid_length, boundary=(grid_min, grid_max), bandwidth, kernel
+        x; npoints=grid_length, boundary=(center_min, center_max), bandwidth, kernel
     )
     grid = k.x
     pdf = k.density
 
     if bound_correction
         # reflect density at the boundary
-        il = firstindex(pdf) + npad_bin
-        ir = lastindex(pdf) - npad_bin
-        pdf[il:(il + nbin_reflect)] .+= @view pdf[il:-1:(il - nbin_reflect)]
-        pdf[(ir - nbin_reflect):ir] .+= @view pdf[(ir + nbin_reflect):-1:ir]
+        il = firstindex(pdf) + npad_left
+        ir = lastindex(pdf) - npad_right
+        pdf[range(il; length=nbin_reflect)] .+= @view pdf[range(
+            il - 1; step=-1, length=nbin_reflect
+        )]
+        pdf[range(ir; step=-1, length=nbin_reflect)] .+= @view pdf[range(
+            ir + 1; length=nbin_reflect
+        )]
     end
 
     # remove padding
-    grid_unpad = grid[(begin + npad_bin):(end - npad_bin)]
-    pdf_unpad = pdf[(begin + npad_bin):(end - npad_bin)]
+    grid = grid[(begin + npad_left):(end - npad_right)]
+    pdf = pdf[(begin + npad_left):(end - npad_right)]
 
-    return KernelDensity.UnivariateKDE(grid_unpad, pdf_unpad)
+    return KernelDensity.UnivariateKDE(grid, pdf)
 end
 
 struct UnivariateCKDE{X,P}
