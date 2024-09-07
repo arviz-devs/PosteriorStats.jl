c4e7c39bdfb7d1e2c3e3f5b08eec1cdf9dd75c53
Seth Axen <seth@sethaxen.com>
2024-09-07 17:11:42 +0200

    Fix and test ETI computation (#37)
    * Correctly compute eti
    
    * Work around quantile erroring for NaNs
    
    * Add tests for eti

--- src/eti.jl
+++ src/eti.jl
@@ -1,7 +1,12 @@
 function eti(x::AbstractVecOrMat{<:Real}; prob::Real=DEFAULT_INTERVAL_PROB)
     0 < prob < 1 || throw(DomainError(prob, "ETI `prob` must be in the range `(0, 1)`."))
     isempty(x) && throw(ArgumentError("ETI cannot be computed for an empty array."))
-    alpha = prob / 2
-    lower, upper = Statistics.quantile(vec(x), (alpha, 1 - alpha))
+    if any(isnan, x)
+        T = float(promote_type(eltype(x), typeof(prob)))
+        lower = upper = T(NaN)
+    else
+        alpha = (1 - prob) / 2
+        lower, upper = Statistics.quantile(vec(x), (alpha, 1 - alpha))
+    end
     return IntervalSets.ClosedInterval(lower, upper)
 end
