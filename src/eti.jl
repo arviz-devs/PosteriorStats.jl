d99100ce7e7f2fe6fa2670806e67e45f9f34ada9
Seth Axen <seth@sethaxen.com>
2024-09-08 18:35:18 +0200

    Add docstrings to eti

--- src/eti.jl
+++ src/eti.jl
@@ -1,3 +1,57 @@
+"""
+    eti(samples::AbstractVecOrMat{<:Real}; [prob, kwargs...]) -> IntervalSets.ClosedInterval
+    eti(samples::AbstractArray{<:Real}; [prob, kwargs...]) -> Array{<:IntervalSets.ClosedInterval}
+
+Estimate the equal-tailed interval (ETI) of `samples` for the probability `prob`.
+
+The ETI of a given probability is the credible interval wih the property that the
+probability of being below the interval is equal to the probability of being above it.
+That is, it is defined by the `(1-prob)/2` and `1 - (1-prob)/2` quantiles of the samples.
+
+See also: [`eti!`](@ref), [`hdi`](@ref), [`hdi!`](@ref).
+
+# Arguments
+- `samples`: an array of shape `(draws[, chains[, params...]])`. If multiple parameters are
+    present
+
+# Keywords
+- `prob`: the probability mass to be contained in the ETI. Default is
+    `$(DEFAULT_INTERVAL_PROB)`.
+- `kwargs`: remaining keywords are passed to `Statistics.quantile`.
+
+# Returns
+- `intervals`: If `samples` is a vector or matrix, then a single
+    `IntervalSets.ClosedInterval` is returned. Otherwise, an array with the shape
+    `(params...,)`, is returned, containing a marginal ETI for each parameter.
+
+!!! note
+    Any default value of `prob` is arbitrary. The default value of
+    `prob=$(DEFAULT_INTERVAL_PROB)` instead of a more common default like `prob=0.95` is
+    chosen to reminder the user of this arbitrariness.
+
+# Examples
+
+Here we calculate the 83% ETI for a normal random variable:
+
+```jldoctest eti; setup = :(using Random; Random.seed!(78))
+julia> x = randn(2_000);
+
+julia> eti(x; prob=0.83)
+-1.3740585250299766 .. 1.2860771129421198
+```
+
+We can also calculate the ETI for a 3-dimensional array of samples:
+
+```jldoctest eti; setup = :(using Random; Random.seed!(67))
+julia> x = randn(1_000, 1, 1) .+ reshape(0:5:10, 1, 1, :);
+
+julia> eti(x)
+3-element Vector{IntervalSets.ClosedInterval{Float64}}:
+ -1.951006825019686 .. 1.9011666217153793
+ 3.048993174980314 .. 6.9011666217153795
+ 8.048993174980314 .. 11.90116662171538
+```
+"""
 function eti(
     x::AbstractArray{<:Real};
     prob::Real=DEFAULT_INTERVAL_PROB,
@@ -7,6 +61,13 @@ function eti(
     return eti!(sorted ? x : _copymutable(x); prob, sorted, kwargs...)
 end
 
+"""
+    eti!(samples::AbstractArray{<:Real}; [prob, kwargs...])
+
+A version of [`eti`](@ref) that partially sorts `samples` in-place while computing the ETI.
+
+See also: [`eti`](@ref), [`hdi`](@ref), [`hdi!`](@ref).
+"""
 function eti!(x::AbstractArray{<:Real}; prob::Real=DEFAULT_INTERVAL_PROB, kwargs...)
     ndims(x) > 0 ||
         throw(ArgumentError("ETI cannot be computed for a 0-dimensional array."))
