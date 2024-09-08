8a091ae40bfe24f4b4710f16b78a8a40d2650440
Seth Axen <seth@sethaxen.com>
2024-09-08 17:56:20 +0200

    Enhance ETI function

--- src/eti.jl
+++ src/eti.jl
@@ -1,12 +1,36 @@
-function eti(x::AbstractVecOrMat{<:Real}; prob::Real=DEFAULT_INTERVAL_PROB)
+function eti(
+    x::AbstractArray{<:Real};
+    prob::Real=DEFAULT_INTERVAL_PROB,
+    sorted::Bool=false,
+    kwargs...,
+)
+    return eti!(sorted ? x : _copymutable(x); prob, sorted, kwargs...)
+end
+
+function eti!(x::AbstractArray{<:Real}; prob::Real=DEFAULT_INTERVAL_PROB, kwargs...)
+    ndims(x) > 0 ||
+        throw(ArgumentError("ETI cannot be computed for a 0-dimensional array."))
     0 < prob < 1 || throw(DomainError(prob, "ETI `prob` must be in the range `(0, 1)`."))
     isempty(x) && throw(ArgumentError("ETI cannot be computed for an empty array."))
+    return _eti!(x, prob; kwargs...)
+end
+
+function _eti!(x::AbstractVecOrMat{<:Real}, prob::Real; kwargs...)
     if any(isnan, x)
         T = float(promote_type(eltype(x), typeof(prob)))
         lower = upper = T(NaN)
     else
         alpha = (1 - prob) / 2
-        lower, upper = Statistics.quantile(vec(x), (alpha, 1 - alpha))
+        lower, upper = Statistics.quantile!(vec(x), (alpha, 1 - alpha); kwargs...)
     end
     return IntervalSets.ClosedInterval(lower, upper)
+end
+function _eti!(x::AbstractArray, prob::Real; kwargs...)
+    axes_out = _param_axes(x)
+    T = float(promote_type(eltype(x), typeof(prob)))
+    interval = similar(x, IntervalSets.ClosedInterval{T}, axes_out)
+    for (i, x_slice) in zip(eachindex(interval), _eachparam(x))
+        interval[i] = _eti!(x_slice, prob; kwargs...)
+    end
+    return interval
 end
--- test/eti.jl
+++ test/eti.jl
@@ -1,23 +1,28 @@
 using IntervalSets
+using OffsetArrays
 using PosteriorStats
 using Statistics
 using Test
 
-@testset "PosteriorStats.eti" begin
+@testset "eti/eti!" begin
     @testset "AbstractVecOrMat" begin
         @testset for sz in (100, 1_000, (1_000, 2)),
             prob in (0.7, 0.76, 0.8, 0.88),
-            T in (Float32, Float64)
+            T in (Float32, Float64, Int64)
 
-            S = Base.promote_eltype(one(T), prob)
             n = prod(sz)
-            x = T <: Integer ? rand(T(1):T(30), sz) : randn(T, sz)
-            r = @inferred PosteriorStats.eti(x; prob)
+            S = Base.promote_eltype(one(T), prob)
+            x = T <: Integer ? rand(T(1):T(30), n) : randn(T, n)
+            r = @inferred eti(x; prob)
             @test r isa ClosedInterval{S}
-            l, u = IntervalSets.endpoints(r)
-            frac_in_interval = mean(∈(r), x)
-            @test frac_in_interval ≈ prob
-            @test count(<(l), x) == count(>(u), x)
+            if !(T <: Integer)
+                l, u = IntervalSets.endpoints(r)
+                frac_in_interval = mean(∈(r), x)
+                @test frac_in_interval ≈ prob
+                @test count(<(l), x) == count(>(u), x)
+            end
+
+            @test eti!(copy(x); prob) == r
         end
     end
 
@@ -25,19 +30,58 @@ using Test
         @testset "NaNs returned if contains NaNs" begin
             x = randn(1000)
             x[3] = NaN
-            @test isequal(PosteriorStats.eti(x), NaN .. NaN)
+            @test isequal(eti(x), NaN .. NaN)
         end
 
         @testset "errors for empty array" begin
             x = Float64[]
-            @test_throws ArgumentError PosteriorStats.eti(x)
+            @test_throws ArgumentError eti(x)
+        end
+
+        @testset "errors for 0-dimensional array" begin
+            x = fill(1.0)
+            @test_throws ArgumentError eti(x)
         end
 
         @testset "test errors when prob is not in (0, 1)" begin
             x = randn(1_000)
             @testset for prob in (0, 1, -0.1, 1.1, NaN)
-                @test_throws DomainError PosteriorStats.eti(x; prob)
+                @test_throws DomainError eti(x; prob)
             end
+        end
+    end
+
+    @testset "AbstractArray consistent with AbstractVector" begin
+        @testset for sz in ((100, 2), (100, 2, 3), (100, 2, 3, 4)),
+            prob in (0.72, 0.81),
+            T in (Float32, Float64, Int64)
+
+            x = T <: Integer ? rand(T(1):T(30), sz) : randn(T, sz)
+            r = @inferred eti(x; prob)
+            if ndims(x) == 2
+                @test r isa ClosedInterval
+                @test r == eti(vec(x); prob)
+            else
+                @test r isa Array{<:ClosedInterval,ndims(x) - 2}
+                r_slices = dropdims(
+                    mapslices(x -> eti(x; prob), x; dims=(1, 2)); dims=(1, 2)
+                )
+                @test r == r_slices
+            end
+
+            @test eti!(copy(x); prob) == r
+        end
+    end
+
+    @testset "OffsetArray" begin
+        @testset for n in (100, 1_000), prob in (0.732, 0.864), T in (Float32, Float64)
+            x = randn(T, (n, 2, 3, 4))
+            xoff = OffsetArray(x, (-1, 2, -3, 4))
+            r = eti(x; prob)
+            roff = @inferred eti(xoff; prob)
+            @test roff isa OffsetMatrix{<:ClosedInterval}
+            @test axes(roff) == (axes(xoff, 3), axes(xoff, 4))
+            @test collect(roff) == r
         end
     end
 end
